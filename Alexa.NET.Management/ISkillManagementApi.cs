5f4f34f9dd5357dd0dd654eb6f2c01d233a2dad1
Steven Pears <steven.pears@live.co.uk>
2017-09-10 15:58:27 +0100

    Fix paths in ISkillManagementApi

--- Alexa.NET.Management/ISkillManagementApi.cs
+++ Alexa.NET.Management/ISkillManagementApi.cs
@@ -1,20 +1,20 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using Refit;
 
 namespace Alexa.NET.Management
 {
     public interface ISkillManagementApi
     {
-        [Get("{skillId}"), Headers("Authorization: Bearer")]
+        [Get("skills/{skillId}"), Headers("Authorization: Bearer")]
         Task<Skill> Get(string skillId);
 
-        [Post("{vendorId}"), Headers("Authorization: Bearer")]
+        [Post("skills/{vendorId}"), Headers("Authorization: Bearer")]
         Task<SkillId> Create(string vendorId, [Body]Skill skill);
 
-        [Put("{skillId}"), Headers("Authorization: Bearer")]
+        [Put("skills/{skillId}"), Headers("Authorization: Bearer")]
         Task<SkillId> Update(string skillId, [Body] Skill skill);
 
-        [Get("{skillId}/status"), Headers("Authorization: Bearer")]
+        [Get("skills/{skillId}/status"), Headers("Authorization: Bearer")]
         Task<SkillStatus> Status(string skillId);
     }
 }
