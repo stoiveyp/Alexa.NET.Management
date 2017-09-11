60c0f8098ec51a6f5878f5b75e26fc8ce62f1140
Steven Pears <steven.pears@live.co.uk>
2017-09-11 22:56:39 +0100

    Add vendor api

--- Alexa.NET.Management/ManagementApi.cs
+++ Alexa.NET.Management/ManagementApi.cs
@@ -39,10 +39,19 @@ namespace Alexa.NET.Management
                 });
 
             InteractionModel = new InteractionModelApi(baseAddress, getToken);
+
+            Vendors = RestService.For<IVendorApi>(
+                baseAddress.ToString(),
+                new RefitSettings
+                {
+                    AuthorizationHeaderValueGetter = getToken
+                });
         }
 
         public ISkillManagementApi Skills { get; set; }
 
         public IInteractionModelApi InteractionModel { get; set; }
+
+        public IVendorApi Vendors { get; set; }
     }
 }
---
+++ Alexa.NET.Management/Vendors/Vendor.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Alexa.NET.Management.Vendors
+{
+    public class Vendor
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("roles")]
+        public string[] Roles { get; set; }
+    }
+}
