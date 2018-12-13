03f9fc2aa9d6c6c5b9d3a9341842eb77f68597bd
Steven Pears <steven.pears@live.co.uk>
2018-12-13 20:04:48 +0000

    ProactiveEvents API Support - Publication in Events - PermissionNames helper class

--- Alexa.NET.Management/Manifest/SkillManifest.cs
+++ Alexa.NET.Management/Manifest/SkillManifest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
@@ -30,11 +31,40 @@ namespace Alexa.NET.Management.Manifest
         [JsonProperty("endpoint")]
         public Endpoint Endpoint { get; set; }
 
-        [JsonProperty("subscriptions")]
+        [JsonProperty("publications",NullValueHandling = NullValueHandling.Ignore)]
+        public Publication[] Publications { get; set; }
+
+        [JsonProperty("subscriptions", NullValueHandling = NullValueHandling.Ignore)]
         public Subscription[] Subscriptions { get; set; }
 
-        [JsonProperty("regions")]
+        [JsonProperty("regions", NullValueHandling = NullValueHandling.Ignore)]
         public Dictionary<string, Endpoint> Regions { get; set; }
+    }
+
+    public class Publication
+    {
+        [JsonProperty("eventName"), JsonConverter(typeof(StringEnumConverter))]
+        public PublicationEventName Name { get; set; }
+    }
+
+    public enum PublicationEventName
+    {
+        [EnumMember(Value="AMAZON.WeatherAlert.Activated")]
+        WeatherAlert,
+        [EnumMember(Value = "AMAZON.SportsEvent.Updated")]
+        SoccerScoreUpdate,
+        [EnumMember(Value = "AMAZON.MessageAlert.Activated")]
+        MessageReminder,
+        [EnumMember(Value = "AMAZON.OrderStatus.Updated")]
+        OrderStatusUpdate,
+        [EnumMember(Value = "AMAZON.Occasion.Updated")]
+        ReservationConfirmation,
+        [EnumMember(Value = "AMAZON.TrashCollectionAlert.Activated")]
+        TrashCollectionReminder,
+        [EnumMember(Value = "AMAZON.SocialGameInvite.Available")]
+        SocialGameInvitation,
+        [EnumMember(Value = "AMAZON.MediaContent.Available")]
+        MediaContentAvailabilityNotification
     }
 
     public class Subscription
@@ -49,6 +79,9 @@ namespace Alexa.NET.Management.Manifest
         SKILL_DISABLED,
         SKILL_PERMISSION_ACCEPTED,
         SKILL_PERMISSION_CHANGED,
-        SKILL_ACCOUNT_LINKED
+        SKILL_ACCOUNT_LINKED,
+        ITEMS_CREATED,
+        ITEMS_UPDATED,
+        ITEMS_DELETED
     }
 }
