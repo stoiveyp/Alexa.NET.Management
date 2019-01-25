d1774fbcdd789bcd5420c8b410c7f1391bbbd0b5
Steven Pears <steven.pears@live.co.uk>
2019-01-25 23:50:42 +0000

    Add request body test

--- Tests/Alexa.NET.Management.Tests/UtteranceProfilerTests.cs
+++ Tests/Alexa.NET.Management.Tests/UtteranceProfilerTests.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Alexa.NET.Management.Api;
 using Alexa.NET.Management.UtteranceProfiler;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace Alexa.NET.Management.Tests
@@ -10,14 +11,32 @@ namespace Alexa.NET.Management.Tests
     public class UtteranceProfilerTests
     {
         [Fact]
-        public async Task AnalyzeCreatesCorrectUrl()
+        public async Task AnalyzeGeneratesCorrectUrl()
         {
             var management = new ManagementApi("xxx", new ActionHandler(req =>
             {
                 Assert.Equal(HttpMethod.Post, req.Method);
                 Assert.Equal("/v1/skills/skillId/stages/development/interactionModel/locales/en-GB/profileNlu", req.RequestUri.PathAndQuery);
             },new UtteranceProfilerResponse()));
-            var response = await management.UtteranceProfiler.Analyze("skillId",SkillStage.DEVELOPMENT,"en-GB","test");
+            await management.UtteranceProfiler.Analyze("skillId",SkillStage.DEVELOPMENT,"en-GB","test");
+        }
+
+        [Fact]
+        public async Task AnalyzeGeneratesCorrectBody()
+        {
+            var management = new ManagementApi("xxx", new ActionHandler(async req =>
+            {
+                var body = JsonConvert.DeserializeObject<UtteranceProfilerRequest>(
+                    await req.Content.ReadAsStringAsync());
+                Assert.Equal("test",body.Utterance);
+                Assert.Equal("test2",body.MultiTurnToken);
+                return new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(JsonConvert.SerializeObject(new UtteranceProfilerResponse()))
+                };
+            }));
+            await management.UtteranceProfiler.Analyze("skillId", SkillStage.DEVELOPMENT, "en-GB", "test","test2");
         }
     }
 }
