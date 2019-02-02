35a50bb5e7eeeafe476287d97d3a58adfa222e25
Gavin Berkowitz <gberkowitz@VOICIFY.COM>
2019-02-02 16:13:06 -0500

    Add custom endpoint type

---
+++ Alexa.NET.Management/Api/CustomApiEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexa.NET.Management.Api
+{
+    public class CustomApiEndpoint
+    {
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
+
+        [JsonProperty("sslCertificateType")]
+        public SslCertificateType SslCertificateType { get; set; }
+    }
+}
