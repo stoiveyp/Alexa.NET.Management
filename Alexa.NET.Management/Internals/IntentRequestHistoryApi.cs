a46d78a440f74bbce323656c7d733bb0adbe9772
Steven Pears <steven.pears@live.co.uk>
2019-06-07 17:36:10 +0100

    Fix IntentRequestHistoryRequest due to complex object mapping in Refit

---
+++ Alexa.NET.Management/Internals/IntentRequestHistoryApi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Alexa.NET.Management.IntentRequestHistory;
+using Refit;
+
+namespace Alexa.NET.Management.Internals
+{
+    internal class IntentRequestHistoryApi:IIntentRequestHistoryApi
+    {
+        public IntentRequestHistoryApi(HttpClient httpClient)
+        {
+            Client = RestService.For<IClientIntentRequestHistoryApi>(httpClient);
+        }
+
+        public IClientIntentRequestHistoryApi Client { get; set; }
+
+        public Task<IntentRequestHistoryResponse> Get(string skillId, IntentRequestHistoryRequest request)
+        {
+            return Client.Get(skillId, new ClientIntentRequestHistoryRequest(request));
+        }
+    }
+}
