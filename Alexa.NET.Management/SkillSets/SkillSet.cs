443a1873ce1b5d9a3deed991e8f040eef6e3554f
Steven Pears <steven.pears@live.co.uk>
2019-06-14 23:49:14 +0100

    Initial skill set

---
+++ Alexa.NET.Management/SkillSets/SkillSet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Alexa.NET.Management.Api;
+using Alexa.NET.Management.Skills;
+
+namespace Alexa.NET.Management.SkillSets
+{
+    public class SkillSet
+    {
+        public SkillSummary Development { get; }
+        public SkillSummary Live { get; }
+        private SkillSet(IEnumerable<SkillSummary> summaries)
+        {
+            foreach (var summary in summaries)
+            {
+                if (summary.Stage == SkillStage.Development)
+                {
+                    Development = summary;
+                }
+                else
+                {
+                    Live = summary;
+                }
+            }
+        }
+
+        public string SkillId => Development.SkillId;
+
+        public static IEnumerable<SkillSet> From(IEnumerable<SkillSummary> summaries)
+        {
+            return summaries.GroupBy(s => s.SkillId).Select(g => new SkillSet(g));
+        }
+    }
+}
---
+++ Tests/Alexa.NET.Management.Tests/SkillSetTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Alexa.NET.Management.Api;
+using Alexa.NET.Management.Skills;
+using Alexa.NET.Management.SkillSets;
+using Xunit;
+
+namespace Alexa.NET.Management.Tests
+{
+    public class SkillSetTests
+    {
+        [Fact]
+        public void CreatesFromSummaryCorrectly()
+        {
+            var skill1 = new SkillSummary { SkillId = "skill1", Stage = SkillStage.Development };
+            var skill2dev = new SkillSummary { SkillId = "skill2", Stage = SkillStage.Development };
+            var skill2live = new SkillSummary { SkillId = "skill2", Stage = SkillStage.Live };
+
+            var skillsets = SkillSet.From(new[] { skill2dev, skill1, skill2live }).ToArray();
+            Assert.Equal(2, skillsets.Length);
+
+            var skill1set = skillsets.First(s => s.SkillId == "skill1");
+            Assert.NotNull(skill1set.Development);
+            Assert.Null(skill1set.Live);
+
+            var skill2set = skillsets.First(s => s.SkillId == "skill2");
+            Assert.NotNull(skill1set.Development);
+            Assert.NotNull(skill1set.Live);
+
+        }
+    }
+}
