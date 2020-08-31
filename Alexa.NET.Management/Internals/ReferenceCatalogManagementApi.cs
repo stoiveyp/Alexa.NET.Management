dd24ba602547d0761e803403216a428523ce527b
Steven Pears <steven.pears@live.co.uk>
2020-08-31 14:31:09 +0100

    Add initial structure

---
+++ Alexa.NET.Management/Internals/ReferenceCatalogManagementApi.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using Alexa.NET.Management.Internals;
+using Refit;
+
+namespace Alexa.NET.Management
+{
+    internal class ReferenceCatalogManagementApi:IReferenceCatalogManagementApi
+    {
+        public ReferenceCatalogManagementApi(HttpClient httpClient)
+        {
+            Client = RestService.For<IClientReferenceCatalogManagementApi>(httpClient, ManagementRefitSettings.Create());
+        }
+
+        public IClientReferenceCatalogManagementApi Client { get; set; }
+    }
+}
