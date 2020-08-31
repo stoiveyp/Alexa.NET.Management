5daf70b81052b0a0e4a1a7f063c74175f02cb865
Steven Pears <steven.pears@live.co.uk>
2020-08-31 14:35:29 +0100

    Referenced catalog management tests

---
+++ Tests/Alexa.NET.Management.Tests/ReferenceCatalogManagementTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alexa.NET.Management.Tests
+{
+    public class ReferenceCatalogManagementTests
+    {
+        [Fact]
+        public void Create()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public void CreateVersion()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public void UpdateStatus()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public void List()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public void ListVersions()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public void Get()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public void GetVersion()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public void GetValues()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public void Update()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public void UpdateVersionInformation()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public void Delete()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        public void DeleteVersion()
+        {
+            Assert.False(true);
+        }
+    }
+}
